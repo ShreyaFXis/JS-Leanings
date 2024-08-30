59dc65bae8fd64668e557526a9fbb6a7c86d272e
Shreya Panchal <shreya.panchal@fxis.ai>
2024-08-30 16:39:35 +0530

    Added Practices

--- basics_3/scopes.js
+++ basics_3/scopes.js
@@ -15,7 +15,7 @@ if (true) {
 
 
 function one(){
-    const username = "hitesh"
+    const username = "Shreya"
 
     function two(){
         const website = "youtube"
@@ -30,8 +30,8 @@ function one(){
 // one()
 
 if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
+    const username = "Shreya"
+    if (username === "Shreya") {
         const website = " youtube"
         // console.log(username + website);
     }
